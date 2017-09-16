ebe02f2516bb1193c3ded1dbaaa964e144f2d491
braverokmc79 <braverokmc79@gmail.com>
2017-09-16 18:28:32 +0900

    자바 변수

--- first/src/first/HelloWorld.java
+++ first/src/first/HelloWorld.java
@@ -3,6 +3,10 @@ package first;
 public class HelloWorld {
 
 	//프로그램의 시작점.
+	/**
+	 *   (1./슬러시 -> 2.별표 두개 -> 3.엔터 )
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 //		System.out.println("test");
