362692ae3f3b2fa57118bb386aaa11d8f6e42288
braverokmc79 <braverokmc79@gmail.com>
2017-09-17 08:57:25 +0900

    10. 기본형

---
+++ first/src/first/PrimitiveDataTypeExam.java
@@ -0,0 +1,12 @@
+package first;
+
+public class PrimitiveDataTypeExam {
+	
+	public static void main(String[] args) {
+		//논리형 리터럴
+		boolean isFun =false;
+		System.out.println(isFun);
+		
+		
+	}
+}
