05280fb6eee4a7b4f027e3c619685430ac4b7dc7
braverokmc79 <braverokmc79@gmail.com>
2017-09-16 18:00:44 +0900

    4. 자바 주석문

--- first/src/first/HelloWorld.java
+++ first/src/first/HelloWorld.java
@@ -2,9 +2,16 @@ package first;
 
 public class HelloWorld {
 
+	//프로그램의 시작점.
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+//		System.out.println("test");
+/*		System.out.println("1  ");
+		System.out.println("2");
+		System.out.println("3");
+*/
+//		System.out.println("단축키 Ctrl+/");
+		/*System.out.println("단축키 Ctrl+Shift/");*/ //블럭단위
 	}
 
 }
