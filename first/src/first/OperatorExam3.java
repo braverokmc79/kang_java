cca956992e9bd624c0ae70b3d46f7d3c45f94713
braverokmc79 <braverokmc79@gmail.com>
2017-09-17 22:40:30 +0900

    19, 연산자우선순위

---
+++ first/src/first/OperatorExam3.java
@@ -0,0 +1,31 @@
+package first;
+
+public class OperatorExam3 {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=10;
+		int c=15;
+		
+		System.out.println(a - b * c); // -145
+		
+		System.out.println( a >5 &&  b >5); //false
+		
+		//System.out.println(++a  - 5); //1
+		System.out.println(a++ -  5);//0
+		
+		System.out.println(a); //6
+	}
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
