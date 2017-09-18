e1c53039d164177a58d05850dcdaffe50659db79
braverokmc79 <braverokmc79@gmail.com>
2017-09-18 14:29:38 +0900

    27. switch 문

---
+++ first/src/part3/SwitchExam.java
@@ -0,0 +1,37 @@
+package part3;
+
+public class SwitchExam {
+
+	public static void main(String[] args) {
+		//switch , case, default, break
+		int value =1;
+		switch(value){ //1출력
+			case 1:
+			  System.out.println("1");
+			  break;
+			case 2:
+				System.out.println("2");
+				break;
+			case 3:
+				System.out.println("3");
+				break;
+			default :
+				System.out.println("그 외 다른 숫자");
+		}
+		String str ="A";
+		switch(str){ //A출력
+			case "A":
+				System.out.println("A");
+				break;
+			case "B" :	
+		}
+	}
+}
+
+
+
+
+
+
+
+
