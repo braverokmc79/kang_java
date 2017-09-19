bc4b3c7d5bbffc03b68c1df2d7e28129d90e2609
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 07:25:21 +0900

    79. 추상클래스

---
+++ first/src/part7/DuckExam.java
@@ -0,0 +1,12 @@
+package part7;
+
+public class DuckExam {
+
+	public static void main(String[] args) {
+		Duck  duck =new Duck();
+		duck.sing();
+		duck.fly();
+		
+		//Bird b=new Bird(); //추상클래스는 객체 생성 불가
+	}
+}
