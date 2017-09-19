d6ebc32f0c5c4b26eb289c9efe35f7e46e53a793
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 04:19:08 +0900

    69.메소드 오버로딩

---
+++ first/src/MethodOverloadExam.java
@@ -0,0 +1,13 @@
+
+public class MethodOverloadExam {
+
+	public static void main(String[] args) {
+		MyClass2 m =new MyClass2();
+		
+		System.out.println(m.plus(4, 5)); //9
+		
+		System.out.println(m.plus(4, 6, 7)); //17
+		
+		System.out.println(m.plus("hello", "world")); //helloworld
+	}
+}
