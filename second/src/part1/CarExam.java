82b77fac87138d1e750923ec807debab89688ea5
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 16:35:59 +0900

    109. java.lang 패키지/오토박싱

---
+++ second/src/part1/CarExam.java
@@ -0,0 +1,10 @@
+package part1;
+
+public class CarExam {
+		
+	public static void main(String[]args){
+		Car ex = new Car();
+		System.out.println(ex);
+	}
+	
+}
---
+++ second/src/part2/WrapperExam.java
@@ -0,0 +1,20 @@
+package part2;
+
+public class WrapperExam {
+	
+	public static void main(String[] args) {
+		int i =5;
+		
+		Integer i2 =new Integer(5);
+		
+		Integer i3 =5; //Auto Boxing
+		
+		int i4 =i3.intValue(); 
+		int i5 =i3;   //Auto unboxing
+	}
+	
+	
+	
+}
+
+
