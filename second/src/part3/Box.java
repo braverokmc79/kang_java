77d9f0503b87c5dbb2483ca3ed36ad7089ba7c8a
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 19:35:35 +0900

    119.generic

---
+++ second/src/part3/Box.java
@@ -0,0 +1,38 @@
+package part3;
+
+/*public class Box {
+	
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+}*/
+
+public class Box<E> {
+	
+	private E obj;
+
+	public E getObj() {
+		return obj;
+	}
+
+	public void setObj(E obj) {
+		this.obj = obj;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
