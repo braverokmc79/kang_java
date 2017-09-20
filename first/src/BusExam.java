072d9d5ef768754cdb8f8ba1894ba6449dec0d43
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 10:04:47 +0900

    86.클래스형변환1

---
+++ first/src/BusExam.java
@@ -0,0 +1,28 @@
+import part7.Bus;
+
+
+public class BusExam {
+	public static void main(String[] args) {
+		Bus bus =new Bus();
+		bus.run();
+	}
+	
+}
+//1.
+//Car 의 기본 생성자 입니다.
+//달리다
+
+//2.
+//오버라이딩 후 =>출력
+//Car 의 기본 생성자 입니다.
+//버스는 달린다
+
+
+
+//3. Bus 클래스 run 메소드에 super.run(); 적용후
+
+//Car 의 기본 생성자 입니다.
+//달리다
+//버스는 달린다
+
+
---
+++ first/src/BusExam2.java
@@ -0,0 +1,20 @@
+import part7.Bus;
+import part7.Car;
+
+//클래스 변환 강의
+public class BusExam2 {
+	
+	public static void main(String[] args) {
+		Car c =new Bus();
+//		c.run();
+		Bus bus =(Bus)c;
+		bus.run();
+		bus.ppangppang();
+	}
+	
+}
+//출력=>
+//Car 의 기본 생성자 입니다.
+//달리다
+//버스는 달린다
+//빵빵
