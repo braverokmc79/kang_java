08727c65fd836e7b21ea7c703d3a71e9e9270fa4
braverokmc79 <braverokmc79@gmail.com>
2017-09-20 17:47:15 +0900

    115. Math

---
+++ second/src/part2/MathExam.java
@@ -0,0 +1,21 @@
+package part2;
+
+public class MathExam {
+	
+	public static void main(String[] args) {
+		int value1 =Math.max(5,  30);
+		System.out.println(value1); //30
+		int value2=Math.min(5, 30);
+		System.out.println(value2); //5
+		System.out.println(Math.abs(-10)); //10
+		System.out.println(Math.random()); //0.875083963009171
+		System.out.println(Math.sqrt(25)); //5.0
+		
+	}
+
+}
+
+
+
+
+
